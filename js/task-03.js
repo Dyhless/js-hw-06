917037100becf530ea17cb5fa894be34a237dc3c
Dmytro Medvediev <dsisitkin@gmail.com>
2023-04-27 17:13:43 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,14 @@
+// Задание 3
+// Напиши скрипт для создания галереи изображений по массиву данных. 
+// В HTML есть список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +23,17 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const imagesGallery = document.querySelector('#gallery');
+
+const imageOne = document.createElement('li');
+imageOne.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageOne.alt = 'White and Black Long Fur Cat';
+
+const imageTwo = document.createElement('li');
+imageTwo.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageTwo.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
+
+const imageThree = document.createElement('li');
+imageThree.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageThree.alt = 'Group of Horses Running';
