74756f6a4c38cea04cfd61de04f5229adeb80e02
Dmytro Medvediev <dsisitkin@gmail.com>
2023-04-27 16:42:58 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -16,58 +16,64 @@ const ingredients = [
   'Condiments',
 ];
 
-// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Создать отдельные элементы <li>. С использованием метода document.createElement().
 
-// Добавит название ингредиента как его текстовое содержимое.
+// Добавить название ингредиента как его текстовое содержимое.
 
-// Добавит элементу класс item.
+// Добавить элементу класс item.
 
-// После чего вставит все <li> за одну операцию в список ul#ingredients.
+// После чего вставить все <li> за одну операцию в список ul#ingredients.
 
 
 // // Создание элементов 
-// const ingredientsEl = document.createElement('ul');
+const ingredientsEl = document.createElement('ul');
 
-// const ingredientPotatoes = document.createElement('li');
-// ingredientPotatoes.classList.add('item');
-// ingredientPotatoes.textContent = 'Potatoes';
+const ingredientPotatoes = document.createElement('li');
+ingredientPotatoes.classList.add('item');
+ingredientPotatoes.textContent = 'Potatoes';
 
-// const ingredientMushrooms = document.createElement('li');
-// ingredientMushrooms.classList.add('item');
-// ingredientMushrooms.textContent = 'Mushrooms';
+const ingredientMushrooms = document.createElement('li');
+ingredientMushrooms.classList.add('item');
+ingredientMushrooms.textContent = 'Mushrooms';
 
-// const ingredientGarlic = document.createElement('li');
-// ingredientGarlic.classList.add('item');
-// ingredientGarlic.textContent = 'Garlic';
+const ingredientGarlic = document.createElement('li');
+ingredientGarlic.classList.add('item');
+ingredientGarlic.textContent = 'Garlic';
 
-// const ingredientTomatos = document.createElement('li');
-// ingredientTomatos.classList.add('item');
-// ingredientTomatos.textContent = 'Tomatos';
+const ingredientTomatos = document.createElement('li');
+ingredientTomatos.classList.add('item');
+ingredientTomatos.textContent = 'Tomatos';
 
-// const ingredientHerbs = document.createElement('li');
-// ingredientHerbs.classList.add('item');
-// ingredientHerbs.textContent = 'Herbs';
+const ingredientHerbs = document.createElement('li');
+ingredientHerbs.classList.add('item');
+ingredientHerbs.textContent = 'Herbs';
 
-// const ingredientCondiments = document.createElement('li');
-// ingredientCondiments.classList.add('item');
-// ingredientCondiments.textContent = 'Condiments';
+const ingredientCondiments = document.createElement('li');
+ingredientCondiments.classList.add('item');
+ingredientCondiments.textContent = 'Condiments';
 
-// ingredientsEl.append(
-//   ingredientPotatoes, ingredientMushrooms, ingredientGarlic, ingredientGarlic, ingredientTomatos, ingredientHerbs, ingredientCondiments
-// );
+ingredientsEl.append(
+  ingredientPotatoes, 
+  ingredientMushrooms, 
+  ingredientGarlic, 
+  ingredientTomatos, 
+  ingredientHerbs, 
+  ingredientCondiments
+);
 
-// console.log(ingredientsEl);
+document.body.appendChild(ingredientsEl);
+console.log(ingredientsEl);
 
 
 // Создание элементов с помошью метода forEach
 
-const ingredientsEl = document.getElementById('ingredients');
+// const ingredientsEl = document.querySelector('#ingredients');
 
-ingredients.forEach((ingredient) => {
-  const li = document.createElement('li');
-  li.textContent = ingredient;
-  li.classList.add('item');
-  ingredientsEl.appendChild(li);
-});
+// ingredients.forEach((ingredient) => {
+//   const li = document.createElement('li');
+//   li.textContent = ingredient;
+//   li.classList.add('item'); 
+//   ingredientsEl.appendChild(li);
+// });
 
-console.log(ingredientsEl);
+// console.log(ingredientsEl);
