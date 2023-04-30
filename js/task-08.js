8e9366e555a15af791e3ad373347ed1e6b6c599b
Dmytro Medvediev <dsisitkin@gmail.com>
2023-04-30 17:21:53 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,43 @@
+// Задание 8
+// Напиши скрипт управления формой логина.
 
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Login</button>
+// </form>
+
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+
+const loginForm = document.querySelector('.login-form');
+// console.dir(loginForm.elements.email);
+// console.dir(loginForm.elements.password); 
+
+loginForm.addEventListener('submit', onSubmit);
+loginForm.elements.email.addEventListener('email', onEmail);
+loginForm.elements.password.addEventListener('password', onPassword);
+
+function onSubmit(event) {
+   event.preventDefault();
+   
+   
+} 
+
+function onEmail(event) {
+   console.dir(loginForm.elements.email.value);
+}
+
+function onPassword(event) {
+   console.dir(loginForm.elements.password.value);
+}
