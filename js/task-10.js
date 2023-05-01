11b54189056b1f8d42b86e2b0bc11e49de0acdbf
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-01 18:02:15 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -15,6 +15,12 @@
 // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
 // Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
 
 const numberInput = document.querySelector('input[type="number"]');
@@ -28,12 +34,6 @@ createBtn.addEventListener('click', () => {
 });
 
 destroyBtn.addEventListener('click', destroyBoxes);
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 
 function createBoxes(amount, boxSize = 30) {
   const boxesToAdd = new Array(amount).fill().map((_, i) => {
